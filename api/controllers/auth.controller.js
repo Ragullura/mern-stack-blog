b41fde86530761b3d26374080e03e360a02ab220
Ragullura <ragullura.005@gmail.com>
2024-03-11 20:39:48 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js"; 
 import bcryptjs from  'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
+//It's for signup page 
 export const signup =async (req,res,next) =>{
    const {username,email,password} = req.body; 
 
@@ -35,7 +37,42 @@ export const signup =async (req,res,next) =>{
     
    }
    
-   
+};
+//------------************--------------------
 
+// It's for sign in 
+
+export const signin =async (req,res,next) => {
+      const { email, password } =req.body;
+      // check all fields
+      if(!email || !password || email ==='' || password===''){
+         next(errorHandler(400,'All fields are required'));
+      }
+
+      try {
+         //it will find the valid email and store it 
+         const validUser =await User.findOne({email});
+
+         if(!validUser){
+            return next(errorHandler(404,'User not found'));
+         }
+         //it will help us to compare the password and then hash the password
+         const validPassword =bcryptjs.compareSync(password,validUser.password);
+
+         if(!validPassword){
+           return next(errorHandler(400,'Invalid password'));
+         }
+
+         const token =jwt.sign({ id:validUser._id}, process.env.JWT_SECRET);
+
+         //we dont want to see hash password also in database for that
+
+         const {password:pass, ...rest} =validUser._doc;
+
+         res.status(200).cookie('access_token', token,{
+            httpOnly:true}).json(rest);//return to validuser using rest
+      } catch (error) {
+         next(error);
+      }
 
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,12 @@
-//Authentication 
+//Authentication  Part
 //===============================================
 
 import express from  'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router =express.Router();
 
 router.post('/signup',signup); //post mean create something
+router.post('/signin',signin);
 
 export default  router;
