60fdcedeaa770e53eac79f9118e6563da637af42
Ragullura <ragullura.005@gmail.com>
2024-03-14 18:16:11 +0530

    complete sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,40 @@
+import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
+import {HiArrowSmRight, HiUser} from  'react-icons/hi';
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSidebar() {
+    const location = useLocation(); //uselocation is used  to access the current URL of our application. 
+    //It gives us an object with properties such as pathname
+    const [tab,setTab] =useState('') //useState  hook is used to declare a state variable for tab and setTab which allows us to update that value, search and hash that we can utilize in
+    useEffect(() => {
+      const urlParams =new  URLSearchParams(location.search);//URLSearchParams allows you to work with query parameters in a URL.
+      const tabFromUrl = urlParams.get('tab');// this will return the value of "?tab=orders" if it exists in the URL or null
+      // if tab  is refreshed or opened directly then it will show
+      if(tabFromUrl){
+        setTab(tabFromUrl);//
+      }
+    }, [location.search]); //location search  will be called every time there's a change in the url after ? (question mark)
+    
+  return (
+    <Sidebar className="w-full md:w-56">
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                <Sidebar.Item  
+                active={tab === 'profile'} 
+                icon={HiUser} 
+                label={"User"} 
+                labelColor='dark' >
+                    Profile
+                </Sidebar.Item>
+                </Link>
+                <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' >
+                    Sign Out
+                </Sidebar.Item>
+                
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const location = useLocation(); //uselocation is used  to access the current URL of our application. 
+  //It gives us an object with properties such as pathname
+  const [tab,setTab] =useState('') //useState  hook is used to declare a state variable for tab and setTab which allows us to update that value, search and hash that we can utilize in
+  useEffect(() => {
+    const urlParams =new  URLSearchParams(location.search);//URLSearchParams allows you to work with query parameters in a URL.
+    const tabFromUrl = urlParams.get('tab');// this will return the value of "?tab=orders" if it exists in the URL or null
+    // if tab  is refreshed or opened directly then it will show
+    if(tabFromUrl){
+      setTab(tabFromUrl);//
+    }
+  }, [location.search]); //location search  will be called every time there's a change in the url after ? (question mark)
+  
+  return( 
+  <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className='md:w-56'>
+      {/* SideBar */}
+        <DashSidebar />
+    </div>
+      {/* Profile and.... */}
+      {/* make a condtion */}
+      {tab === 'profile' && <DashProfile/> }
+  </div>
+  );
 }
