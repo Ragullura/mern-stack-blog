88e9167702897aeb6925c5775114dbdfedff7124
Ragullura <ragullura.005@gmail.com>
2024-03-15 19:55:40 +0530

    Complete user Image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,21 +1,111 @@
-import { Button, TextInput } from "flowbite-react";
-import {  useSelector } from "react-redux"
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
+import {  useSelector } from "react-redux";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from  "firebase/storage";
+import { app } from '../firebase';
+
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
     const {currentUser} = useSelector(state => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null); //set  to current user image if no new file is uploaded temperory 
+    const filePickerRef =useRef(); //useRef for  the button to be able to click on it programmatically
+    const [imageFileUploadProgress, setImageFileUploadProgress] =useState(null); // to keep track of how much progress is made in
+    const [imageFileUploadError, setImageFileUploadError]= useState(null); // to store any error that might occur during image upload
+    const handleImageChange= (e) =>{
+        const file = e.target.files[0];
+        if(file){
+            setImageFile(file)  //set  image file to the selected one
+            //change into file into url  for previewing
+            setImageFileUrl(URL.createObjectURL(file));  
+        }
+       
+    };
+    useEffect ( () =>{
+        if(imageFile){
+            uploadImage();
+        }
+    }, [imageFile]);
+
+    const uploadImage =async () =>{
+
+        setImageFileUploading(true); // set that we are currently uploading an image
+        setImageFileUploadError(null); // resetting error message when a new image is uploaded
+        const storage    = getStorage(app);
+        const fileName   = new Date().getTime()+imageFile.name;
+        const storageref = ref(storage,fileName); //creating a reference in firebase to store our images
+        const uploadTask = uploadBytesResumable(storageref , imageFile);//uploading the file using the method provided by Firebase
+        uploadTask.on(
+            'state_changed',
+            (snapshot)=>{
+                // Observe state change events such as progress, pause, or resume
+                const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) *100;
+                setImageFileUploadProgress(progress.toFixed(0));//showing the percentage of how much is uploaded and  tofixed is used for rounded the value 20.33 to 20
+            },
+            (error) => {
+                setImageFileUploadError('Could not upload image(File must be less then 2MB)')
+
+                setImageFileUploadProgress(null);// remove the circular loader when error occurs
+                setImageFile(null); //remove the image from display once there's been an error
+                setImageFileUrl(null); // remove the image URL so no broken image link will show up
+                
+            },
+            () =>
+             {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>{
+                    setImageFileUrl(downloadURL);
+            });  
+             }
+        );
+    };
+    
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
         <h1 className="my-7 text-center font-bold text-3xl font-Georgia">Profile</h1>
         <form className="flex flex-col gap-4">
-            <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" >
-                <img src={currentUser.profilePicture} alt="user" 
-                className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"  />
+            < input type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} hidden />
+            
+            <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" 
+            onClick={() => filePickerRef.current.click()} >
+                {imageFileUploadProgress && (
+                    <CircularProgressbar 
+                        value={imageFileUploadProgress || 0}
+                        text={`${imageFileUploadProgress}%`}
+                        strokeWidth={5}
+                        styles={{
+                            root:{
+                                width:'100%',
+                                height:'100%',
+                                position:'absolute',
+                                top:0,
+                                left:0,
+                            },
+                            path: {
+                                stroke:`rgba(62,152,199, ${
+                                    imageFileUploadProgress /100
+                                })`,
+                            },
+                        }} 
+                    />
+                )}
+                <img src={imageFileUrl || currentUser.profilePicture} alt="user" 
+                className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+                    imageFileUploadProgress &&
+                    imageFileUploadProgress < 100 &&
+                    'opacity-60'
+                  }`} />
 
             </div>
+                {imageFileUploadError && (<Alert color='failure'>{imageFileUploadError}</Alert>)  }
+                
+
                 <TextInput type="text" id="username" placeholder="username" 
-                defaultValue={currentUser.username}/>
+                defaultValue={String(currentUser.username)}/>
                 <TextInput type="email" id="email" placeholder="email" 
-                defaultValue={currentUser.email}/>
+                defaultValue={String(currentUser.email)}/>
                 <TextInput type="password" id="password" placeholder="password" />
 
                 <Button type="submit" gradientDuoTone='purpleToBlue' outline>
@@ -27,5 +117,5 @@ export default function DashProfile() {
             <span className="cursor-pointer">Sign Out</span>
         </div>
     </div>
-  )
+  );
 }
