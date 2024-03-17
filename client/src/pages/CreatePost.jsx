bb296a08522417f940c071722a0d7c4a56aee7fe
Ragullura <ragullura.005@gmail.com>
2024-03-17 16:29:44 +0530

    complete upload post image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,62 @@
 import {  Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+  } from 'firebase/storage';
+  import { app } from '../firebase';
+  import { CircularProgressbar } from 'react-circular-progressbar';
+  import 'react-circular-progressbar/dist/styles.css';
 
 export default function CreatePost() {
+    const [file, setFile] = useState(null);
+    const [imageUploadProgress, setImageUploadProgress] = useState(null);
+    const [imageUploadError, setImageUploadError] = useState(null);
+    const [formData, setFormData] = useState({}); //it  will store our form data
+
+    const handleUpdloadImage = async () => {
+            try {
+                if (!file) {
+                    setImageUploadError('Please select an image'); 
+                    return;
+                  }
+                  setImageUploadError(null);
+                  const storage = getStorage(app);
+                  const fileName = new Date().getTime() + '-' + file.name;
+                  const storageRef = ref(storage, fileName);
+                  const uploadTask = uploadBytesResumable(storageRef, file);
+                  uploadTask.on(
+                    'state_changed',
+                    (snapshot) => {
+                      const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                      setImageUploadProgress(progress.toFixed(0));
+                    },
+                    (error) => {
+                        setImageUploadError('Image upload failed');
+                        setImageUploadProgress(null);
+                      },
+                      // we use url  to store the download URL of our uploaded image in the database
+                      () => {
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                          setImageUploadProgress(null);
+                          setImageUploadError(null);
+                          setFormData({ ...formData, image: downloadURL });
+                        });
+                      }
+
+                  );  
+                  
+            } catch (error) {
+                setImageUploadError('Image upload failed');
+                setImageUploadProgress(null);
+                console.log(error);
+            }
+    }
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a new post</h1>
@@ -29,18 +83,40 @@ export default function CreatePost() {
           <FileInput
             type='file'
             accept='image/*'
-            
+            onChange={(e) => setFile(e.target.files[0])}
           />
           <Button
             type='button'
             gradientDuoTone='purpleToBlue'
             size='sm'
             outline
-            
+            onClick={handleUpdloadImage}
+            disabled={imageUploadProgress}
           >
-            Upload
+           {imageUploadProgress ? (
+              <div className='w-16 h-16'>
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              'Upload Image'
+            )}
           </Button>
         </div>
+        {/* show error */}
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+
+        {/* show image  preview if there is an uploaded file*/}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt='upload'
+            className='w-full h-72 object-cover'
+          />
+        )}
+
 
         <ReactQuill
           theme='snow'
